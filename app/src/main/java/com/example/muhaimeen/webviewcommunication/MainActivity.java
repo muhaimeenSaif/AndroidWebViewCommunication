b4c5f1b54d4df0fad3848933ec71977903b69749
muhaimeenSaif <muhaimeen.ahmed@gmail.com>
2019-01-27 16:19:57 -0500

    Working with web view
    Passing data from android to web page using webView

---
+++ app/src/main/java/com/example/muhaimeen/webviewcommunication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.muhaimeen.webviewcommunication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    //private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WebView webView = new WebView(MainActivity.this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        // Add your local url here//
+        webView.loadUrl("http://192.168.0.147/messaging/deliverBae/WebViewTest.html"); //not in scope of this gist
+        webView.setWebViewClient(new WebViewClient(){
+            public void onPageFinished(WebView view, String url){
+                //Here you want to use .loadUrl again
+                //on the webview object and pass in
+                //"javascript:<your javaScript function"
+                webView.loadUrl("javascript:myJavaScriptFunc('argumentPassingIn')"); //if passing in an object. Mapping may need to take place
+            }
+        });
+        setContentView(webView);
+
+    }
+}
